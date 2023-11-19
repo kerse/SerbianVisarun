9689ebf29771f7123171b636c23fd52b4cc48714
Kirill Tomilov <kersent@gmail.com>
2023-11-20 01:25:04 +0300

    Fix datepicker

--- datepicker.js
+++ datepicker.js
@@ -1,55 +1,82 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
-    const daysContainer = document.getElementById('days');
-    const monthDisplay = document.getElementById('month');
+    const monthSelector = document.getElementById('monthSelector');
+    const daySelector = document.getElementById('daySelector');
+    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
 
-    let currentMonth = new Date().getMonth();
-    let currentYear = new Date().getFullYear();
-
-    function generateDays(month, year) {
-        daysContainer.innerHTML = ''; // Очистить текущие дни
-        let date = new Date(year, month, 1);
-        while (date.getMonth() === month) {
-            const dayElement = document.createElement('div');
-            dayElement.classList.add('day');
-            dayElement.textContent = date.getDate();
-            daysContainer.appendChild(dayElement);
-            date.setDate(date.getDate() + 1);
-        }
-        monthDisplay.textContent = date.toLocaleString('ru', { month: 'long' }) + ' ' + year;
-    }
-
-    generateDays(currentMonth, currentYear);
-
-    let startX;
-
-    daysContainer.addEventListener('touchstart', (e) => {
-        startX = e.touches[0].clientX;
+    months.forEach(month => {
+        const monthElement = document.createElement('div');
+        monthElement.classList.add('scroll-item');
+        monthElement.textContent = month;
+        monthSelector.appendChild(monthElement);
     });
 
-    daysContainer.addEventListener('touchmove', (e) => {
-        const moveX = e.touches[0].clientX;
-        const diffX = moveX - startX;
-
-        if (Math.abs(diffX) > 50) { // Пороговое значение для свайпа
-            if (diffX > 0) {
-                // Свайп вправо - предыдущий месяц
-                if (currentMonth === 0) {
-                    currentMonth = 11;
-                    currentYear -= 1;
-                } else {
-                    currentMonth -= 1;
-                }
-            } else {
-                // Свайп влево - следующий месяц
-                if (currentMonth === 11) {
-                    currentMonth = 0;
-                    currentYear += 1;
-                } else {
-                    currentMonth += 1;
-                }
-            }
-            generateDays(currentMonth, currentYear);
-            startX = null; // Сбросить значение startX
+    function generateDays(monthIndex) {
+        daySelector.innerHTML = '';
+        let date = new Date(new Date().getFullYear(), monthIndex, 1);
+        while (date.getMonth() === monthIndex) {
+            const dayElement = document.createElement('div');
+            dayElement.classList.add('scroll-item');
+            dayElement.textContent = date.getDate();
+            daySelector.appendChild(dayElement);
+            date.setDate(date.getDate() + 1);
         }
+    }
+
+    generateDays(new Date().getMonth());
+
+    function selectItemInCenter(container, itemClass) {
+        let containerRect = container.getBoundingClientRect();
+        let center = containerRect.left + containerRect.width / 2;
+
+        let items = container.getElementsByClassName(itemClass);
+        let selectedItem = null;
+        Array.from(items).forEach(item => {
+            let itemRect = item.getBoundingClientRect();
+            if (itemRect.left < center && itemRect.right > center) {
+                selectedItem = item;
+            }
+            item.classList.remove('selected');
+        });
+        if (selectedItem) {
+            selectedItem.classList.add('selected');
+        }
+    }
+
+    monthSelector.addEventListener('scroll', () => {
+        selectItemInCenter(monthSelector, 'scroll-item');
+    });
+
+    daySelector.addEventListener('scroll', () => {
+        selectItemInCenter(daySelector, 'scroll-item');
+    });
+
+    setTimeout(() => {
+        selectItemInCenter(monthSelector, 'scroll-item');
+        selectItemInCenter(daySelector, 'scroll-item');
+    }, 100);
+
+    [monthSelector, daySelector].forEach(selector => {
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+
+        selector.addEventListener('mousedown', (e) => {
+            isDown = true;
+            startX = e.pageX - selector.offsetLeft;
+            scrollLeft = selector.scrollLeft;
+        });
+        selector.addEventListener('mouseleave', () => {
+            isDown = false;
+        });
+        selector.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+        selector.addEventListener('mousemove', (e) => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - selector.offsetLeft;
+            const walk = (x - startX) * 3;
+            selector.scrollLeft = scrollLeft - walk;
+        });
     });
 });
